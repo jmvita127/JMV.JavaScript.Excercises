fc160db394233c87e8d586b0c4c9d8db1a65a26e
jmvita127 <jmvita127@gmail.com>
2019-07-02 10:58:37 -0300

    JS excercises

--- JS.EventosForm/script.js
+++ JS.EventosForm/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function(){
 
   var input = document.querySelector('input');
-  // var button = document.querySelector('button'); // este captura el boton HOLA
-  var addTask = document.querySelector('form button');
+  // var button = document.querySelector('button'); // captura el boton HOLA
+  var addTask = document.querySelector('form button'); // captura el boton del form
   //console.log(input.getAttribute('value'));
   var ul = document.querySelector('ul');
 
