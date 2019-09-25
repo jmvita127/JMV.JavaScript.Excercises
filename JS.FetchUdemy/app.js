e0dfb0012710f9e0843a9dcb31527d7247d3e4e3
jmvita127 <jmvita127@gmail.com>
2019-09-25 11:25:03 -0300

    JS.FecthTryCatchUdemy

--- JS.FetchUdemy/app.js
+++ JS.FetchUdemy/app.js
@@ -1,20 +1,80 @@
 // fetch(peticiones ajax) y peticiones a servicios / apis rest
 
 var div_usuarios = document.querySelector('#usuarios');
+var div_janet = document.querySelector('#janet');
 
-var usuarios = [];
+ getUsuarios()
+.then(data => data.json())
+.then(users => {
+  listadoUsuarios(users.data);
+  return getUsuario();
 
-fetch('https://reqres.in/api/users')
-  .then(data => data.json())
-  .then(users => {
-    usuarios = users.data;
-    console.log(usuarios);
+})
+.then(data => data.json())
+.then(user => {
+  mostrarJanet(user.data);
 
-    usuarios.map((user, i)=>{
-      let nombre = document.createElement('h2');
+  return getInfo();
 
-      nombre.innerHTML = i + user.first_name + ' ' + user.last_name;
+})
+.then(data => {
+  console.log(data);
+})
 
-      div_usuarios.appendChild(nombre);
+.catch(error => {
+  console.log(error);
+});
+
+function getUsuarios()
+{
+  return fetch('https://reqres.in/api/users');
+}
+
+function getUsuario() //con esta peticion obtengo solo un unico usuario - Janet
+{
+  return fetch('https://reqres.in/api/users/2');
+}
+
+function getInfo() // crear promesa desde cero.
+{
+
+    var profesor = {
+      nombre: 'Victor',
+      apellido: 'Robles',
+      url:'https://victorroblesweb.es'
+    };
+
+    return new Promise((resolve, reject) =>{
+      var profesor_string = JSON.stringify(profesor);
+
+      if (typeof profesor_string != 'string') return reject('ERROR');
+      return resolve(profesor_string);
+
     });
-  });
+
+}
+
+function listadoUsuarios(usuarios)
+{
+usuarios.map((user, i) => {
+  let nombre = document.createElement('h2');
+
+  nombre.innerHTML = i + ' ' + user.first_name + ' ' + user.last_name;
+
+  div_usuarios.appendChild(nombre);
+ });
+}
+
+function mostrarJanet(user)
+{
+  console.log(user);
+  let nombre = document.createElement('h3');
+  let avatar = document.createElement('img');
+
+  nombre.innerHTML = 'Usuario encontrado: ' + user.first_name + ' ' + user.last_name;
+  avatar.src = user.avatar
+  avatar.width = '100';
+
+  div_janet.appendChild(nombre);
+  div_janet.appendChild(avatar);
+}
---
+++ JS.FetchUdemy/try-catch.js
@@ -0,0 +1,13 @@
+try {
+
+  var year = 2019;
+
+  alert(year);
+  console.log(decodeURIComponent('https://v$%ctorrobles.web.es'));
+  var vector = new Array(99999999999999999999999999);
+
+
+} catch (error) {
+  console.log(error);
+  alert('Ha ocurrido un error en el codigo');
+}
